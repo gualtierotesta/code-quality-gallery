efcd2ddbeb5a13ece40338b51d46638dbfe58508
Gualtiero Testa <gualtiero.testa@gmail.com>
2015-06-12 20:50:01 +0200

    (F) First version

---
+++ src/main/java/it/gualtierotesta/codequalitygallery/Sample.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2015 Gualtiero Testa <www.gualtierotesta.it>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package it.gualtierotesta.codequalitygallery;
+
+/**
+ *
+ * @author Gualtiero Testa <www.gualtierotesta.it>
+ */
+public class Sample {
+
+    public String myMethod() {
+        String str = null;
+        return str.toUpperCase();
+    }
+
+}
